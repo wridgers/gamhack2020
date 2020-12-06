fa86661c5f8d6dc3e4a530ae637cddf7d533a0e2
David Buckley <davidbuckley@gambitresearch.com>
2020-12-06 20:42:33 +0000

    use threads

---
+++ bots/random_play/__init__.py
@@ -0,0 +1,22 @@
+import json
+import random
+import sys
+from .. import base
+
+class Player(base.Player):
+	def run(self):
+		header = self.receive()
+
+		version = header['gen']
+		rounds = header['rounds']
+
+		self.send({
+			'ready': True,
+		})
+
+		for _ in range(rounds):
+			round_header = self.receive()
+
+			self.send({
+				'hand': [random.choice(['R', 'P', 'S'])],
+			})
--- engine.py
+++ engine.py
@@ -1,3 +1,4 @@
+import importlib
 import json
 import logging
 import os
@@ -6,8 +7,8 @@ import re
 import sqlite3
 import subprocess
 import sys
-
-import pexpect
+import threading
+import queue
 
 from db import setupdb, save_result
 
@@ -134,6 +135,40 @@ class P1FoulException(Exception):
 class P2FoulException(Exception):
 	pass
 
+TIMEOUT = 0.1
+
+class PlayerThread(threading.Thread):
+	def __init__(self, player_num, player_module):
+		self.player_in_queue = queue.Queue(maxsize=1)
+		self.player_out_queue = queue.Queue(maxsize=1)
+		self.player_module = player_module
+		self.player = player_module.Player(self.player_in_queue, self.player_out_queue)
+		self.player_num = player_num
+		self.exception_class = [P1FoulException, P2FoulException][player_num - 1]
+		super().__init__(daemon=True)
+
+	def run(self):
+		self.player.run()
+
+	def join(self):
+		super().join(timeout=TIMEOUT)
+		if self.is_alive():
+			raise self.exception_class('timed out on exit')
+
+	def send(self, obj):
+		try:
+			return self.player_in_queue.put(obj, timeout=TIMEOUT)
+		except queue.Full:
+			raise self.exception_class('timed out on write')
+
+	def receive(self):
+		try:
+			return self.player_out_queue.get(timeout=TIMEOUT)
+		except queue.Empty:
+			raise self.exception_class('timed out on read')
+
+	def __str__(self):
+		return "%s(%s, %s)" % (self.__class__.__name__, self.player_num, self.player_module)
 
 class Engine:
 	TIMEOUT = 100
@@ -159,22 +194,6 @@ class Engine:
 			'round': rnd,
 		}
 
-	@staticmethod
-	def write_json(p, obj):
-		data = json.dumps(obj)
-		LOGGER.info("Wrote: %s", data)
-		got = p.read(p.sendline(data))
-		LOGGER.info("Got: %s", got)
-
-	@staticmethod
-	def read_json(p):
-		'''
-		TODO: timeout + failure
-		'''
-		data = p.readline()
-		LOGGER.info("Data read: %s", data)
-		return json.loads(data)
-
 	def get_players(self):
 		module_re = re.compile('^[a-z0-9][a-z0-9_]+$')
 		with os.scandir('bots') as it:
@@ -192,6 +211,7 @@ class Engine:
 		players = list(self.get_players())
 		random.shuffle(players)
 		while len(players) > 1:
+			LOGGER.info("NEW ROUND; players=%s", players)
 			next_players = []
 			for i in range(len(players) // 2):
 				round_players = players[i*2:i*2+2]
@@ -213,39 +233,44 @@ class Engine:
 				break
 			players = next_players
 
+	@staticmethod
+	def make_player(player_num, player_name):
+		module = importlib.import_module('bots.' + player_name)
+		return PlayerThread(player_num, module)
+
 	def run_pairing(self, player_names):
+		assert len(player_names) == 2
+		LOGGER.info('p1: %s, p2: %s', *player_names)
+
+		players = []
+		for idx, player_name in enumerate(player_names):
+			player = self.make_player(idx + 1, player_name)
+			player.start()
+			players.append(player)
+
 		try:
-			assert len(player_names) == 2
-			LOGGER.info('p1: %s, p2: %s', *player_names)
-
-			players = [
-				pexpect.spawn('python -u -m bots.%s' % (player_name,), timeout=self.TIMEOUT / 1e3)
-				for player_name in player_names
-			]
-
 			game = self.game_class(self.rounds)
 			for player in players:
-				self.write_json(player, self.game_header(player_names))
+				player.send(self.game_header(player_names))
 
 			for idx, player in enumerate(players):
-				assert self.read_json(player)['ready']
+				assert player.receive()['ready']
 				LOGGER.info('p%s ready', idx + 1)
 
 			for i in range(self.rounds):
 				for player in players:
-					self.write_json(player, self.round_header(i + 1))
+					player.send(self.round_header(i + 1))
 
 				moves = []
 				for player in players:
-					moves.append(self.read_json(player)['hand'][0])
+					moves.append(player.receive()['hand'][0])
 
 				LOGGER.info('p1_move=%r, p2_move=%r', *moves)
 
 				game.apply(*moves)
 
-			for player in players:
-				player.expect(pexpect.EOF)
-				player.kill(0)
+			for idx, player in enumerate(players):
+				player.join()
 
 			scores = game.final_scores()
 
