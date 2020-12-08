4578f0c74cf9c13a8f3bdc8d7d72d42183437671
David Buckley <davidbuckley@gambitresearch.com>
2020-12-08 21:33:52 +0000

    end when there is a player winner

--- arena.sh
+++ arena.sh
@@ -28,5 +28,10 @@ do
 		# update results on index.html ?
 	) |& tee ./www/logs/${fight_id}.txt
 
+	winner=$(sqlite3 hack.db "select player from tournament_results where elimination_round = 0 and player like 'team%' order by tournament_id limit 1")
+	if [ -n "$winner" ]; then
+		break
+	fi
+
 	sleep 1
 done
