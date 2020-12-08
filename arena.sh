affda6a1f5effa7913b6b3e676ef9030f146dd36
Will Ridgers <will@mindfuzz.net>
2020-12-08 22:34:24 +0000

    arena: rename variable

--- arena.sh
+++ arena.sh
@@ -8,10 +8,10 @@ mkdir -p ./www/logs
 
 while :
 do
-	fight_id=$(date +%s)
+	tournament_id=$(date +%s)
 
 	(
-		echo "Begin round $fight_id"
+		echo "Begin round $tournament_id"
 		for i in `seq 1 7`; do
 			echo "Checking out team$i"
 			(
@@ -22,11 +22,11 @@ do
 			)
 		done
 
-		# run a fight, .txt so the browser can show it without triggering a download (dirty hack)
-		python3 ./engine.py ${fight_id}
+		# run a tournament, .txt so the browser can show it without triggering a download (dirty hack)
+		python3 ./engine.py ${tournament_id}
 
 		# update results on index.html ?
-	) |& tee ./www/logs/${fight_id}.txt
+	) |& tee ./www/logs/${tournament_id}.txt
 
 	winner=$(sqlite3 hack.db "select player from tournament_results where elimination_round = 0 and player like 'team%' order by tournament_id limit 1")
 	if [ -n "$winner" ]; then
