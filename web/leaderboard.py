faf502b8af0f0ae375db6b3de596a59d9bd0d827
Gamhack <you@example.com>
2020-12-09 15:01:53 +0000

    official tournament results

--- web/leaderboard.py
+++ web/leaderboard.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import datetime as dt
 import sqlite3
 
@@ -17,6 +18,13 @@ OFFICIAL_LEADERBOARD_QUERY = '''
 	join official using (tournament_id)
 	group by 1
 	order by 2 desc
+'''
+
+OFFICIAL_RESULTS_QUERY = '''
+	select tournament_id, elimination_round, player
+	from tournament_results
+	join official using (tournament_id)
+	order by 1, 2
 '''
 
 TEMPLATE = '''<html>
@@ -73,6 +81,28 @@ TEMPLATE = '''<html>
 			</tbody>
 		</table>
 
+		<h2>Individual Tournament Results</h2>
+
+		{% for tournament in tournament_results %}
+		<h3>{{ tournament.tournament_id }}</h3>
+		<table>
+			<thead>
+				<tr>
+					<th>Final Round</th>
+					<th>Players</th>
+				</tr>
+			</thead>
+			<tbody>
+				{% for round in tournament.elimination_rounds %}
+					<tr>
+						<td>{{ loop.index }}</td>
+						<td>{{ round }}</td>
+					</tr>
+				{% endfor %}
+			</tbody>
+		</table>
+		{% endfor %}
+
 		<p>generated {{ now }}</p>
 	</body>
 </html>
@@ -88,12 +118,23 @@ def main():
 	cur.execute(OFFICIAL_LEADERBOARD_QUERY)
 	official_leaderboard = cur.fetchall()
 
+	cur.execute(OFFICIAL_RESULTS_QUERY)
+	tournament_eliminations = defaultdict(lambda: defaultdict(list))
+	for tournament_id, elimination_round, player in cur:
+		tournament_eliminations[tournament_id][elimination_round].append(player)
+	tournament_results = []
+	for tournament_id, rounds in sorted(tournament_eliminations.items(), reverse=True):
+		tournament_result = []
+		for elimination_round in range(0, -5, -1):
+			tournament_result.append(', '.join(rounds[elimination_round]))
+		tournament_results.append({'tournament_id': tournament_id, 'elimination_rounds': tournament_result})
+
 	conn.commit()
 	conn.close()
 
 	template = Template(TEMPLATE)
 
-	print(template.render(leaderboard=leaderboard, official_leaderboard=official_leaderboard, now=str(dt.datetime.now())))
+	print(template.render(leaderboard=leaderboard, official_leaderboard=official_leaderboard, tournament_results=tournament_results, now=str(dt.datetime.now())))
 
 
 if __name__ == '__main__':
